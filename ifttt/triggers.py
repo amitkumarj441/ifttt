75851a2f49409e4f2656955e3f29a3023a26e785
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2016-04-17 18:42:42 +0530

    Update triggers.py
    Extended the BaseTriggerClass

--- ifttt/triggers.py
+++ ifttt/triggers.py
@@ -30,6 +30,7 @@ import logging
 
 import flask
 import flask.views
+from flask import g, render_template, make_response, request
 
 import feedparser
 import werkzeug.contrib.cache
@@ -47,7 +48,8 @@ from utils import (select,
                     utc_to_epoch,
                     utc_to_iso8601,
                     iso8601_to_epoch,
-                    find_hashtags)
+                    find_hashtags,
+                    snake_case)
 
 LOG_FILE = 'ifttt.log'
 CACHE_EXPIRATION = 5 * 60
@@ -146,7 +148,28 @@ class BaseTriggerView(flask.views.MethodView):
 
     def get_data(self):
         pass
-
+    
+    def get(self):
+        """Handle GET requests."""
+        # Creating the feed's name
+        feed_name = snake_case(self.__class__.__name__)
+        self.fields = {}
+        self.params = flask.request.get_json(force=True, silent=True) or {}
+        self.limit = self.params.get('limit', DEFAULT_RESP_LIMIT)
+        feed_identity = self.params.get('feed_identity')
+        feed_values = self.params.get('feedFields', {"lang":"en"})
+        feed_values = self.params.get('feedFields', {"lang":request.args.get('lang')})
+        for field, default_value in self.default_fields.items():
+            self.fields[field] = feed_values.get(field)
+            if self.fields[field] == '' and default_value not in TEST_FIELDS:  
+        data = self.get_data()
+        data = data[:self.limit]
+        feeds = render_template('article_of_the_day_feeds.xml', data=data)
+        feeds = render_template('feed_name.xml', data=data)
+        response = make_response(feeds)
+        response.headers["Content-Type"] = "application/xml"
+    
+    
     def post(self):
         """Handle POST requests."""
         self.fields = {}
